9675fd65756fbdde315a518648723854cf4435b1
Adam S Johnson <adam.johnson.main@gmail.com>
2018-08-20 17:39:54 -0400

    Okay

--- TagRemover.py
+++ TagRemover.py
@@ -9,7 +9,9 @@ def main():
 	targetRoot = configFile.readline().strip()
 	subDirs = configFile.readline().strip().split(',')
 	grabPat = configFile.readline().strip()
-	pattern = re.compile(grabPat)
+	pattern = "false"
+	if(grabPat != "false"):
+		pattern = re.compile(grabPat)
 	season = configFile.readline().strip()
 	tags = [line.strip() for line in configFile]
 	#get the dirs to parse files through
@@ -20,24 +22,26 @@ def main():
 				fullPath = '%s/%s' % (path, file)
 				for tag in tags:
 					file = file.replace(tag, '')
-				numbers = pattern.findall(file)
-				s = "{:02}".format( int( season))
-				e = "{:02}".format(int(numbers[0]))
-				
-				file = file.replace(numbers[0], 'S%sE%s' % (s,e )).strip()
+				numbers = "false"
+				if( pattern != 'false' and season != 'false'):
+					numbers = pattern.findall(file)
+					s = "{:02}".format( int( season))
+					e = "{:02}".format(int(numbers[0]))
+					
+					file = file.replace(numbers[0], 'S%sE%s' % (s,e )).strip()
 				replaceFile =  '%s/%s' % (path, file)
 				
 				#our always remove list is just ()[] just since they are a pain
 				for x in ALWAYS_REMOVE:
 					replaceFile = replaceFile.replace(x, '')
-				if(len(numbers) > 1):
+				if(numbers != "false" and len(numbers) > 1):
 					print("Could not determine episode number")
 					print(fullPath)
 					print("Modify by hand!!!")
 				else:
 					print(numbers)
 					print(file)
-					os.rename(fullPath, replaceFile)
+					#os.rename(fullPath, replaceFile)
 
 
 
